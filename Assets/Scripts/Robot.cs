421fa425f64ca9cdb9b0a5f19d190761f64ee9fc
Majid Moghadam <majid.moghadam114@yahoo.com>
2019-10-10 00:15:10 -0400

    added robot animations: rolling & firing

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -15,6 +15,8 @@ public class Robot : MonoBehaviour
     private float timeLastFired;
     private bool isDead;
 
+    public Animator robot;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -45,6 +47,6 @@ public class Robot : MonoBehaviour
     }
     private void fire()
     {
-        Debug.Log("Fire");
+        robot.Play("Fire");
     }
 }
