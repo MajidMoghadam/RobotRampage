a67b9a5d5497ef3968ac56c5b5f922098fcfa329
Majid Moghadam <majid.moghadam114@yahoo.com>
2019-10-14 00:56:43 -0400

    added some sound effects

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -10,6 +10,13 @@ public class Robot : MonoBehaviour
     [SerializeField]
     GameObject missileprefab;
 
+    [SerializeField]
+    private AudioClip deathSound;
+    [SerializeField]
+    private AudioClip fireSound;
+    [SerializeField]
+    private AudioClip weakHitSound;
+
     public int health;
     public int range;
     public float fireRate;
@@ -55,6 +62,8 @@ public class Robot : MonoBehaviour
         missile.transform.position = missileFireSpot.transform.position;
         missile.transform.rotation = missileFireSpot.transform.rotation;
         robot.Play("Fire");
+
+        GetComponent<AudioSource>().PlayOneShot(fireSound);
     }
 
     // 1
@@ -70,6 +79,11 @@ public class Robot : MonoBehaviour
             isDead = true;
             robot.Play("Die");
             StartCoroutine("DestroyRobot");
+            GetComponent<AudioSource>().PlayOneShot(deathSound);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(weakHitSound);
         }
     }
     // 2
